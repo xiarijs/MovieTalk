9f35c5c8a7233bc8ab9cfa9ef21a112d4d09995a
xiarijs <563727334@qq.com>
2017-05-18 15:43:02 +0800

    change icons

--- index.android.js
+++ index.android.js
@@ -24,26 +24,42 @@ const REQUEST_MOVIES_URL = 'http://api.douban.com/v2/movie/top250';
 export default class MovieTalk extends Component {
   constructor(props){
     super(props);
+
+    this.state = {
+      onTabbar: 'rank'
+    }
   }
 
   render(){
     return(
-      <TabNavigator>
+      <TabNavigator tabBarStyle={{backgroundColor:'darkslateblue'}}>
         <TabNavigator.Item
-          selected={true}
+          selected={this.state.onTabbar == 'rank'}
           title='电影排行'
-          renderIcon={() => <Image size={30} source={{uri: icons.star}} />}
-          renderSelectedIcon={() => <Image size={30} source={{uri: icons.star}} />}
+          renderIcon={() => <Image style={{width:23,height:23,tintColor:'#929292'}} source={{uri: icons.star}} />}
+          renderSelectedIcon={() => <Image style={{width:23,height:23,tintColor:'white'}} source={{uri: icons.starActive}} />}
+          onPress={()=>{
+            this.setState({
+              onTabbar: 'rank'
+            })
+          }}
+          selectedTitleStyle={{color: 'white'}}
         >
-        <MovieList />
+          <MovieList />
         </TabNavigator.Item>
         <TabNavigator.Item
-          selected={false}
+          selected={this.state.onTabbar == 'usa'}
           title='北美票房'
-          renderIcon={() => <Image size={30} source={{uri: icons.star}} />}
-          renderSelectedIcon={() => <Image size={30} source={{uri: icons.star}} />}
+          renderIcon={() => <Image style={{width:20,height:20,tintColor:'#929292'}} source={{uri: icons.rank}} />}
+          renderSelectedIcon={() => <Image style={{width:20,height:20,tintColor:'white'}} source={{uri: icons.rankActive}} />}
+          onPress={()=>{
+            this.setState({
+              onTabbar: 'usa'
+            })
+          }}
+          selectedTitleStyle={{color: 'white'}}
         >
-        <USBox />
+          <USBox />
         </TabNavigator.Item>
       </TabNavigator>
       //<MovieList />
